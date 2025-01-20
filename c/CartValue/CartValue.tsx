97bc3ec6841abcc0f14f17a1937bb1326c8b68ce
healingdrawing <healingdrawing@gmail.com>
2025-01-20 22:57:21 +0200

    refactor validation

--- c/CartValue/CartValue.tsx
+++ c/CartValue/CartValue.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react'
 import { useAtom } from 'jotai'
 import { cart_value_atom } from '../../utils/atoms'
-import { custom_alert_for_float_input } from '../../utils/alert'
 import { simulate_tab_event } from '../../utils/tab'
 
 const CartValue: React.FC = () => {
@@ -11,31 +10,30 @@ const CartValue: React.FC = () => {
   /** Validating float input, for EUR with cents */
   const currency_regex = /^(\d+)?(\.)?(\d{1,2})?$/
 
-  /** to allow input, without black magic. -.5 etc */
-  const wait_list = ['.','-','-.']
+  /** to allow input, without black magic .5 etc */
+  const wait_list = ['','.']
 
   const handle_input_change = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
-    if (currency_regex.test(value) || value === '') {
-      if (!wait_list.includes(value))
-      {
-        set_cart_value(value === '' ? null : parseFloat(value))
-      }
-    } else {
-      e.target.value = cart_value === null ? '' : cart_value.toString()
-    }
+    if (wait_list.includes(value)) return
+    else if (currency_regex.test(value)) set_cart_value(parseFloat(value))
+    else e.target.value = cart_value === null ? '0' : cart_value.toString()
   }
 
   const handle_input_on_blur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const value = e.target.value
-    if (!currency_regex.test(value) && value !== '') {
-      e.target.value = ''
-      custom_alert_for_float_input(e)
+    if (wait_list.includes(e.target.value)){
+      set_cart_value(0)
+      e.target.value
     }
   }
 
   const handle_key_up = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
+      console.log("keyup value", e.currentTarget.value)
+      if (e.currentTarget.value === ''){
+        set_cart_value(0)
+        e.currentTarget.value = '0'
+      }
       simulate_tab_event(e.currentTarget)
     }
   }
